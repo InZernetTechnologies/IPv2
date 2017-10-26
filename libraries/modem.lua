b9eb08765902d02ffe47c48139b1cd8ff60875f1
Cody Robinson <lifewcody@gmail.com>
2017-10-26 04:24:28 -0400

    ADDED - modem

---
+++ libraries/modem.lua
@@ -0,0 +1,66 @@
+local moduleInformation = {
+	name = "modem",
+	version = "1.0.0",
+}
+
+local sideTable = {
+    [1] = "bottom",
+    [2] = "top",
+    [3] = "back",
+    [4] = "front",
+    [5] = "right",
+    [6] = "left",
+    [ "bottom" ] = 1,
+    [ "top" ] = 2,
+    [ "back" ] = 3,
+    [ "front" ] = 4,
+    [ "right" ] = 5,
+    [ "left" ] = 6
+}
+
+function getSides()
+    return sideTable
+end
+
+function getMAC(side)
+  if not side then error("No side given",2) end
+  side = tostring(side)
+  if getSides()[side] then 
+    local macBuffer = tostring(DecToBase(os.computerID() * 6 + getSides()[side],16))
+    local MACaddr = string.rep("0",12-#macBuffer).. macBuffer
+    return MACaddr
+  end
+end
+
+function DecToBase(val,base)
+	if val == 0 then return 0 end
+	local b, k, result, d = base or 10, "0123456789ABCDEFGHIJKLMNOPQRSTUVW",""
+	while val > 0 do
+		val, d = math.floor(val/b), math.fmod(val,b)+1
+		result = string.sub(k,d,d)..result
+	end
+	return result
+end
+
+local function getActiveSides()
+	local as = {} -- Create empty table
+	for s=1, 6 do -- For each side on the computer: right, back, etc
+		if peripheral.getType(rs.getSides()[s]) == "modem" then -- If something is on that side and it is a modem
+			as[rs.getSides()[s]] = peripheral.call(rs.getSides()[s], "isWireless") -- Set the table key to the side and set it to if it's wireless (true = wireless, false = modem)
+		end
+	end
+	return as -- Return the table, empty or has entries
+end
+
+-- REQUIRED MODULE FUNCTIONS
+function getModuleInformation()
+    return moduleInformation
+end
+
+function load()
+	
+end
+
+function unload()
+    
+end
