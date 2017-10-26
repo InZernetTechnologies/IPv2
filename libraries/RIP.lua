810818480b151519d61e122914e4c004122b9e53
Cody Robinson <lifewcody@gmail.com>
2017-10-26 07:17:58 -0400

    ADDED - RIP module

---
+++ libraries/RIP.lua
@@ -0,0 +1,21 @@
+local moduleInformation = {
+    name = "RIP",
+    version = "1.0.0"
+}
+
+function process(packet)
+    
+end
+
+-- REQUIRED MODULE FUNCTIONS
+function getModuleInformation()
+    return moduleInformation
+end
+
+function load()
+    
+end
+
+function unload()
+
+end
