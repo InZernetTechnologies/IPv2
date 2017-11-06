444d7ea1b511c9b5c3e2572733f278a53113f1d3
Cody Robinson <lifewcody@gmail.com>
2017-11-06 10:29:43 -0500

    ADDED - loop, interval, sleep duration

--- tests/sendFrame.lua
+++ tests/sendFrame.lua
@@ -1,5 +1,25 @@
-local goodFrame = {
-    "000000000005",
+local goodFrameWithGoodIPv2 = {
+    "000000000013",
+    "000000000003",
+    2048,
+    {
+        2,
+        0,
+        17,
+        16,
+        "1.1",
+        "1.2",
+        {
+            "this",
+            "is",
+            "my",
+            "data",
+        }
+    }
+}
+
+local goodBroadcastFrame = {
+    "FFFFFFFFFFFF",
     "000000000003",
     2048,
     {
@@ -27,5 +47,12 @@ write("What side do you want to conduct the test? > ")
 local s = io.read()
 
 local modem = peripheral.wrap(s)
-modem.transmit(1, 1, goodFrame)
+write("How many times? > ")
+local times = io.read()
+write("Interval (s) > ")
+local interval = tonumber(io.read())
+for i=1,times do
+    modem.transmit(1, 1, goodBroadcastFrame)
+    sleep(interval)
+end
 print("Sent!")
