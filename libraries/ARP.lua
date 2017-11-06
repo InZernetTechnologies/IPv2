576c1ea6e4aca5ef2f87ec9f5aa5356d0a9a29bb
Cody Robinson <lifewcody@gmail.com>
2017-11-06 10:30:23 -0500

    ADDED - ARP

---
+++ libraries/ARP.lua
@@ -0,0 +1,47 @@
+local moduleInformation = {
+    name = "ARP",
+    version = "1.0.0"
+}
+
+local ARPCache = {}
+
+function set(KEY, VALUE)
+    ARPCache[KEY] = VALUE
+end
+
+function get(KEY)
+    if (ARPCache[KEY] ~= nil) then
+        return ARPCache[KEY]
+    else
+        return false
+    end
+end
+
+function isGood(packet)
+    if (packet[1] == nil) return false end
+    if (packet[2] == nil) return false end
+    return true
+end
+
+function handle(packet)
+    if tonumber(packet[5]) == 1 then
+
+    elseif tonumber(packet[5] == 2) then
+
+    else
+    
+    end
+end
+
+-- REQUIRED MODULE FUNCTIONS
+function getModuleInformation()
+    return moduleInformation
+end
+
+function load()
+    
+end
+
+function unload()
+
+end
